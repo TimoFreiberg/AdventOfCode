25c7551d52bae7573717247d0df8b48b0346808b
Timo Freiberg <self@timofreiberg.com>
2021-12-02 09:00:03 +0100

    Add test

--- aoc2021/src/day1.rs
+++ aoc2021/src/day1.rs
@@ -2,7 +2,7 @@ use eyre::Result;
 
 use crate::input;
 
-pub fn solve() -> Result<(String, String)> {
+pub fn solve() -> Result<(usize, usize)> {
     let input = input(1);
     let depths: Vec<_> = input
         .lines()
@@ -12,7 +12,7 @@ pub fn solve() -> Result<(String, String)> {
     Ok((part1(&depths), part2(&depths)))
 }
 
-fn part2(depths: &[u32]) -> String {
+fn part2(depths: &[u32]) -> usize {
     let windows = depths
         .iter()
         .zip(&depths[1..])
@@ -22,11 +22,20 @@ fn part2(depths: &[u32]) -> String {
     part1(&windows)
 }
 
-fn part1(depths: &[u32]) -> String {
+fn part1(depths: &[u32]) -> usize {
     depths
         .iter()
         .zip(&depths[1..])
         .filter(|(first, second)| first < second)
         .count()
-        .to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day1() {
+        assert_eq!(solve().unwrap(), (1681, 1704))
+    }
 }
