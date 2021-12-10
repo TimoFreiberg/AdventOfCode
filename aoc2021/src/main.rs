0b3a51133cbd1281152ad598a2c545d9334ac557
Timo Freiberg <self@timofreiberg.com>
2021-12-10 17:31:08 +0100

    Day10

--- aoc2021/src/main.rs
+++ aoc2021/src/main.rs
@@ -9,6 +9,7 @@ mod day6;
 mod day7;
 mod day8;
 mod day9;
+mod day10;
 
 fn main() {
     tracing_subscriber::fmt::init();
@@ -21,6 +22,7 @@ fn main() {
     println!("day7: {:?}", day7::solve().unwrap());
     println!("day8: {:?}", day8::solve().unwrap());
     println!("day9: {:?}", day9::solve().unwrap());
+    println!("day10: {:?}", day10::solve().unwrap());
 }
 
 fn input(day: usize) -> String {
