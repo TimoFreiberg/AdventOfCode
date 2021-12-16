3e60e1ae4727124b61bd5cc7a79479c136c8363a
Timo Freiberg <self@timofreiberg.com>
2021-12-16 18:11:12 +0100

    14

--- aoc2021/src/main.rs
+++ aoc2021/src/main.rs
@@ -5,6 +5,7 @@ mod day10;
 mod day11;
 mod day12;
 mod day13;
+mod day14;
 mod day2;
 mod day3;
 mod day4;
@@ -31,6 +32,7 @@ fn main() {
     let (day13_1, day13_2) = day13::solve().unwrap();
     println!("day13.1: {:?}", day13_1);
     println!("day13.2:\n{}", day13_2);
+    println!("day14: {:?}", day14::solve().unwrap());
 }
 
 fn input(day: usize) -> String {
